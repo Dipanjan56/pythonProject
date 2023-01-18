f113bddd1d0322f35425da394475938a931a01b4
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-01-19 00:39:46 +0530

    python learning

--- head_first_python_tutorial/basic.py
+++ head_first_python_tutorial/basic.py
@@ -5,6 +5,25 @@ print(word)
 
 # -----------------------------------------
 
+# bool value of 0, None, empty string or any empty data structure returns False
+
+print('bool value of 0:', bool(0))
+print('bool value of None:', bool(None))
+print('bool value of empty string:', bool(''))
+print('bool value of empty data structure:', bool({}))
+
+# bool value of any number except 0, non-empty string pr any data-structure with values  returns True
+
+
+print('bool value of 1:', bool(1))
+print('bool value of -1:', bool(-1))
+print('bool value of None:', bool('asd'))
+print('bool value of empty string:', bool({1,2,3}))
+
+
+
+# -----------------------------------------
+
 # if else
 
 y = 5
--- head_first_python_tutorial/functions_and_modules.py
+++ head_first_python_tutorial/functions_and_modules.py
@@ -1,9 +1,16 @@
 # functions: take some line of code , give them a name and you 've got a function.
-# function is initialised by 'def some_name():'
+# function is initialised by  keyword 'def some_name():'
+# function introduce two new keyword: 'def' and 'return'
+# function can accept argument
+# function contain code and usually documentation
 
 # module: take a collection of function and package them as a file and you've got a module
 
 # standard library: collection of modules
+
+# we can define the type of argument and return type by using annotations
+# e.g def function_name(parameter: type) -> return type:
+# function annotations are optional
 
 # -----------------------------------------
 
@@ -13,8 +20,63 @@ from datetime import date
 date_today = date.today()
 print(date_today)
 
+
 # here datetime: standard library, date: module, today(): function
 
 # -----------------------------------------
+# create your own function
+
+def convert_number_to_string(n):
+    """converting a integer to string"""
+    return str(n)
 
 
+# -----------------------------------------
+
+# invoking a function
+
+print(convert_number_to_string(5))
+
+
+# -----------------------------------------
+
+# example: identify vowels in string
+
+def search_vowels(word):
+    vowels = set('aeiou')
+    return vowels.intersection(set(word))
+
+
+print(search_vowels('hello'))
+print(search_vowels('sky'))
+
+
+# -----------------------------------------
+
+# example: if vowels in string then true otherwise false
+
+def search_vowels_2(word):
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+    return bool(found)
+
+
+print(search_vowels_2('hello'))
+print(search_vowels_2('sky'))
+
+# -----------------------------------------
+
+# we can define the type of argument and return type by using annotations
+# e.g def function_name(parameter: type) -> return type:
+
+def search_vowels_3(word: str) -> set:
+    vowels = set('aeiou')
+    return vowels.intersection(set(word))
+
+
+def search_vowels_4(word: str) -> bool:
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+    return bool(found)
+
+# -----------------------------------------
