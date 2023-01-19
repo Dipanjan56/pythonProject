6a879268f2f236d4ecd2feb24205a54582abe435
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-01-20 01:44:31 +0530

    python learning

--- head_first_python_tutorial/basic.py
+++ head_first_python_tutorial/basic.py
@@ -1,3 +1,23 @@
+# We think - Python support both call-by-value and call-by-reference for function arguments
+# [see example in page 184 -187 (Head First Python)]
+
+# In python variables are not like the variables like other programming languages,
+# here variable means object reference, i.e. it is useful to think of the value stored in the variable as being the
+# memory address of the value, not its actual value. Its the memory address that passed into the funtion,
+# not the actual value
+# Hence, Python's function support what's ,ore correctly called call-by-reference call semantics
+
+# if the argument value is mutable, then call-by-reference apply
+# if the argument value is immutable, then call-by-value apply
+
+
+# importing a module in current project
+import functions_and_modules
+
+print(functions_and_modules.convert_number_to_string(1))
+
+# -----------------------------------------
+
 # input from console/ user input
 
 word = input('Provide a word: ')
@@ -18,9 +38,7 @@ print('bool value of empty data structure:', bool({}))
 print('bool value of 1:', bool(1))
 print('bool value of -1:', bool(-1))
 print('bool value of None:', bool('asd'))
-print('bool value of empty string:', bool({1,2,3}))
-
-
+print('bool value of empty string:', bool({1, 2, 3}))
 
 # -----------------------------------------
 
@@ -42,5 +60,3 @@ print(f'if else: x = {x}, y = {y}')
 x = 10 if y > 6 else 20
 
 print(f'if else using ternary operator: x = {x}, y = {y}')
-
-
---
+++ head_first_python_tutorial/module_to_site_packages/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='util_functions',
+    version='1.0',
+    description='The Head First Python Utility tools',
+    author='Dipanjan Kundu',
+    author_email='dipanjan56@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['util_functions']
+)
