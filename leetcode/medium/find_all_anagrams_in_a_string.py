466af2cbf41a04eb292d2e0e36cadd2c099f77b6
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-02-07 02:24:40 +0530

    python learning

--- leetcode/medium/find_all_anagrams_in_a_string.py
+++ leetcode/medium/find_all_anagrams_in_a_string.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import itertools
+
 """Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -31,7 +34,6 @@ Constraints:
 
 1 <= s.length, p.length <= 3 * 104
 s and p consist of lowercase English letters."""
-from collections import Counter
 
 """This is the optimized one"""
 """Solution: use sliding window approach and make use of Counter ds from collections library
@@ -58,7 +60,13 @@ def findAnagrams(s: str, p: str):
     print(index_list)
 
 
+def find_all_possible_anagram(s: str):
+    perms = [''.join(perm) for perm in itertools.permutations('abc')]
+    print(perms)
+
+
 if __name__ == '__main__':
     s = "cbaebabacd"
     p = "abc"
     findAnagrams(s, p)
+    find_all_possible_anagram('abcd')
