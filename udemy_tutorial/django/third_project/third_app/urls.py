f8f6117afbc6645a6c4bc4e41653ad439fcc4e41
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-02-13 19:35:50 +0530

    python learning

--- udemy_tutorial/django/third_project/third_app/urls.py
+++ udemy_tutorial/django/third_project/third_app/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.example_view), path('variable/', views.variable_view)]
+# register in app namespace
+# URL NAMES
+app_name = 'third_app'
+urlpatterns = [
+    path('', views.example_view, name='example'),
+    path('variable/', views.variable_view, name='variable'),
+    path('inherited/', views.inherited_view, name='inherited')
+]
--- udemy_tutorial/django/third_project/third_app/views.py
+++ udemy_tutorial/django/third_project/third_app/views.py
@@ -8,7 +8,14 @@ def example_view(request):
 
 def variable_view(request):
     third_app_var = {'first_name': 'Dipanjan',
-                     'last_name': 'Kundu',
-                     'some_list': [1, 2, 3],
-                     'some_dict': {'inside_key': 'inside_variable'}}
+                     'last_name': 'kuNdu',
+                     'some_list': [1, 2, 3, 4],
+                     'some_dict': {'inside_key': 'inside_variable'},
+                     'user_logged_in': True}
     return render(request, 'third_app/variable.html', context=third_app_var)
+
+
+def inherited_view(request):
+    return render(request, 'third_app/inherited.html')
+
+
