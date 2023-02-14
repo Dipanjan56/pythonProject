a87d125a459cc4d09ee2a4f7456ce2af98720fb7
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-02-15 02:20:35 +0530

    python learning

--- udemy_tutorial/django/django_basics_documentation.py
+++ udemy_tutorial/django/django_basics_documentation.py
@@ -31,15 +31,39 @@ Run App:
 terminal ->  python3 manage.py runserver
 
 Step 5
-make migration for installed apps in settings.py with project level:
+Database creation and integration:
+it will create db.sqllite3 in your project and
+make migration for installed apps/ tables in DB in settings.py with project level:
 terminal ->  python3 manage.py migrate
 
 Step 6
 add go to apps.py in app level and copy th class_name : e.g ThirdAppConfig
 then add like this 'third_app.apps.ThirdAppConfig' in installed_apps in project level settings.py
 
-Step 6
+Step 7
 terminal -> python3 manage.py makemigrations third_app
 
+
+Note:
+    Migration: In general, migration is the act of connecting changes in your Django project or app to the database
+               This includes things like adding models within app, ading new app, updating models
+               with a new column/attribute and more
+
+               we typically manage these command in manage.py file once you create model ->
+               1. makemigrations: it sets the instruction to perform the update/create activity,
+                                  but does not create on its own
+               2. migrate: we need to then run this command to perform the activity
+               3. sqlmigrate : if u wanted to see what sql code look like, the run this command
+
+    Command DB: Patient.object.get(pk=1) -> here pk means Primary Key and
+                always starts from 1 not 0 like array positioning
+
+
+Step 8:
+create superuser [admin]:
+terminal -> python3 manage.py createsuperuser
+username: admin
+email: dipanjan56@gmail.com
+password: Test@123
 
 """
