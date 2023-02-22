d281565ca74bb93e66a30b3776a94bdf1b5cb7e2
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-02-22 13:38:40 +0530

    python learning

--- leetcode/easy/best_time_to_buy_sell_stock.py
+++ leetcode/easy/best_time_to_buy_sell_stock.py
@@ -1,5 +1,6 @@
-"""
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
+"""Best Time To Buy Sell Problem"""
+
+"""You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
@@ -28,15 +29,50 @@ Constraints:
 0 <= prices[i] <= 104
 """
 from typing import List
+import math
+
+"""using two pointers approach
+Time Complexity is O(n^2)
+"""
 
 
 def maxProfit(prices: List[int]) -> int:
-    """need to solve it"""
+    max_profit = 0
+    for i in range(len(prices) - 1):
+        current_profit = 0
+        for j in range(i + 1, len(prices)):
+            current_profit = prices[j] - prices[i]
+            if current_profit < 0:
+                break
+            if current_profit > max_profit:
+                max_profit = current_profit
 
-    return 0
+    return max_profit
+
+
+"""using single pointer
+Time complexity is O(n)
+"""
+
+
+def max_profit_optimized(prices: List[int]) -> int:
+    min_price = math.inf
+    max_profit = 0
+    for i in range(len(prices)):
+        current_profit = 0
+        if prices[i] < min_price:
+            min_price = prices[i]
+        else:
+            current_profit = prices[i] - min_price
+        if current_profit > max_profit:
+            max_profit = current_profit
+
+    return max_profit
 
 
 if __name__ == '__main__':
     prices_1 = [7, 1, 5, 3, 6, 4]
     prices_2 = [2, 4, 1]
-    print(maxProfit(prices_2))
+    prices_3 = [7, 2, 5, 8, 4, 1, 3, 6]
+    print(maxProfit(prices_3))
+    print(max_profit_optimized(prices_3))
