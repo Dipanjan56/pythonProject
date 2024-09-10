36aed0162f069e92ac00d275e2a1a2c0ff91c38a
dipanjan <dipanjan56@gmail.com>
2024-09-10 16:55:05 +0530

    added new code

--- coding_platform/competitive_programming/search/binary_search.py
+++ coding_platform/competitive_programming/search/binary_search.py
@@ -7,28 +7,44 @@
 4. it has practical and real world applications as O(logN) running time is quite favourable - 
    it is close to O(1) constant running time
 """
+
 from typing import List
 
 
-def binary_search(num_list: List[int], item, start, end) -> int:
-    # 1st base case: if item is not present in the container
-    if end < start:
-        return -1
-
+def binary_search_recursion(arr: List[int], start, end, target):
     mid = (start + end) // 2
 
-    # 2nd base case: if we find the item in the middle
-    if num_list[mid] == item:
+    if target == arr[mid]:
         return mid
+    elif target < arr[mid]:
+        return binary_search_recursion(arr, start, mid - 1, target)
+    elif target > arr[mid]:
+        return binary_search_recursion(arr, mid + 1, end, target)
+    else:
+        return -1
 
-    # if item is less than middle_num, then we can get the item in the left side of the sub_array
-    # as this is a sorted list and hence we can discard the right side of the array
-    if item < num_list[mid]:
-        return binary_search(num_list, item, start, mid - 1)
-    # if item is greater than middle_num, then we can get the item in the right side of the sub_array
-    # as this is a sorted list and hence we can discard the left side of the array
-    elif item > num_list[mid]:
-        return binary_search(num_list, item, mid + 1, end)
+
+def binary_search_iteration(arr: List[int], target):
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            end = mid - 1
+        elif target > arr[mid]:
+            start = mid + 1
+    return -1
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5, 6]
+    x = 3
+    print(f'binary_search_recursion: number {x} found at index: {binary_search_recursion(nums, 0, len(nums) - 1, x)}')
+    print(f'binary_search_iteration: number {x} found at index: {binary_search_iteration(nums, x)}')
 
 
 if __name__ == '__main__':
