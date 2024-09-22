2db0bc3e0f8f309b5affd9a66024460e2e054e61
dipanjan <dipanjan56@gmail.com>
2024-09-22 14:02:26 +0530

    added new code

--- coding_platform/neetcode/arrays&hashing/medium/products_of_array_discluding_self.py
+++ coding_platform/neetcode/arrays&hashing/medium/products_of_array_discluding_self.py
@@ -27,11 +27,10 @@ Constraints:
 
 from typing import List
 
-
+"""
+Time Complexity : O(n^2)
+"""
 def productExceptSelf(nums: List[int]) -> List[int]:
-    if len(nums) == 2:
-        return nums[::-1]
-
     product_list = []
     for i in range(len(nums)):
         product = 1
@@ -43,6 +42,40 @@ def productExceptSelf(nums: List[int]) -> List[int]:
     return product_list
 
 
+"""
+Approach:
+
+	1.	Prefix Product: For each index i, calculate the product of all the elements before i (i.e., to the left of i).
+	2.	Suffix Product: Similarly, for each index i, calculate the product of all the elements after i (i.e., to the right of i).
+	3.	Result: Multiply the prefix product and the suffix product for each index i to get the desired output for output[i].
+
+Solution Steps:
+
+	1.	Initialize Two Arrays:
+	•	One array for the prefix products.
+	•	Another array for the suffix products.
+	2.	Calculate Prefix Products:
+	•	Traverse the array from left to right, and for each element, store the product of all previous elements.
+	3.	Calculate Suffix Products:
+	•	Traverse the array from right to left, and for each element, store the product of all the elements to its right.
+	4.	Compute the Output:
+	•	Multiply the corresponding prefix and suffix products for each index to get the result.
+	
+	Time Complexity : O(n)
+"""
+def productExceptSelf_optimized(nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+
 if __name__ == '__main__':
     nums = [1,2,4,6]
     print(productExceptSelf(nums))
+    print(productExceptSelf_optimized(nums))
